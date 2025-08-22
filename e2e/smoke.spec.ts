e7ff164531b5897132051844a107238d1611f063
Miguel Padin <miguel.padin.dominguez@gmail.com>
2025-08-22 13:07:42 +0100

    test(e2e): split smoke test into multiple checks for improved visibility

--- e2e/smoke.spec.ts
+++ e2e/smoke.spec.ts
@@ -1,6 +1,18 @@
 import { test, expect } from "@playwright/test";
 
-test("home renders", async ({ page }) => {
+test("home renders 1", async ({ page }) => {
+  console.log("smoke test");
+  await page.goto("/");
+  await expect(page.locator("#app")).toBeVisible();
+});
+
+test("home renders 2", async ({ page }) => {
+  console.log("smoke test");
+  await page.goto("/");
+  await expect(page.locator("#app")).toBeVisible();
+});
+
+test("home renders 3", async ({ page }) => {
   console.log("smoke test");
   await page.goto("/");
   await expect(page.locator("#app")).toBeVisible();
